2e1c880f63c73fcd380b8485d191d5f508871b90
Christian <chriscar.sam@gmail.com>
2023-09-23 13:08:58 -0500

    Conditions in Go

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/chriscarsam/gofrom0/variables"
+	"runtime"
 )
 
 func main() {
-	fmt.Println(variables.ConvertText(1544))
+	// fmt.Println(variables.ConvertText(1544))
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X" {
+		fmt.Println("This is", os)
+	} else {
+		fmt.Println("This is", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Printf("This is %s \n", os)
+	case "darwin":
+		fmt.Printf("This is %s \n", os)
+	default:
+		fmt.Printf("This is %s \n", os)
+	}
 }
